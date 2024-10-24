a783445217771b34f09f7f27b7aa6ff0db1d6256
Your Name <jannat27120@gmail.com>
2024-10-25 02:33:37 +0600

    Refactor eslint.config.js and add styling to Countries component

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import "./Country.css";
 const Country = ({ country }) => {
     // console.log(country);
-    const { name, flags } = country;
+    const { name, flags, population, area, cca3 } = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(true);
+    };
 
     return (
         <div className="country">
             <h3>Name: {name?.common}</h3>
             <img src={flags.png} alt="flag" />
+            <p>Population: {population}</p>
+            <p>Area: {area} km²</p>
+            <p>
+                <small>Code: {cca3}</small>
+            </p>
+            <button onClick={handleVisited}>Visited</button>
+            {visited && <p>I have visited {name?.common}</p>}
         </div>
     );
 };
