6f1474c55ca2ddde7ab8665279f542b3044ec0f3
miftah1299 <jannat27120@gmail.com>
2024-10-25 11:52:21 +0600

    Refactor Countries component CSS and add visited status functionality

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./Country.css";
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry }) => {
     // console.log(country);
     const { name, flags, population, area, cca3 } = country;
 
@@ -9,9 +9,10 @@ const Country = ({ country }) => {
     const handleVisited = () => {
         setVisited(!visited);
     };
+    console.log(handleVisitedCountry);
 
     return (
-        <div className="country">
+        <div className={`country ${visited && "visited"}`}>
             <h3>Name: {name?.common}</h3>
             <img src={flags.png} alt="flag" />
             <p>Population: {population}</p>
@@ -19,10 +20,12 @@ const Country = ({ country }) => {
             <p>
                 <small>Code: {cca3}</small>
             </p>
+
+            <button>Mark Visited</button>
             <button onClick={handleVisited}>
                 {visited ? "Visited" : "Going"}
             </button>
-            
+
             {visited ? (
                 <p>I have visited {name?.common}</p>
             ) : (
